6625bcbd18e1b759e66ab400a69771272e0a1a03
NilukWellalage <wnt@tiqri.com>
2023-01-24 22:26:43 +0530

    Login changes

---
+++ Authentication.Model/LoginResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Authentication.Model
+{
+    public class LoginResponse
+    {
+        [JsonPropertyName("accessToken")]
+        public string AccessToken { get; set; }
+    }
+}
